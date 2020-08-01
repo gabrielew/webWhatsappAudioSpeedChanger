cdd2e9c74d528de60cbef2464ce1543421a70d47
gabrielew <gabrielalcantaratp@gmail.com>
2020-07-31 21:42:57 -0300

    adding save in localstorage

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ## A chrome extension to change web whatsapp audio speed to 2x
 
-<p> This extension is for those who don't like to listen to five-minute audios every time, so next to the profile photo a 2x button will appear, and even better, you won't have to worry about changing the speed all the time, the state will be saved after your first choice! </p>
+<p> This extension is for those who don't like to listen to five-minute audios every time, so next to the profile photo a 2x button will appear, <strong>and even better</strong>, you won't have to worry about changing the speed all the time, the state will be saved after your first choice! </p>
 
 
 <p> To use in Google Chrome you will need to clone or download and drag the folder to <strong>chrome://extensions</strong>, remember to active developer mode in the right side header menu. </p>
--- content.js
+++ content.js
@@ -2,29 +2,55 @@ const interval = setInterval(() => {
   const header = document.querySelector("._1QUKR");
   if (header) {
     clearInterval(interval);
-
-    let playbackRate = 1;
-
     const button = document.createElement("button");
-    button.innerHTML = "2x";
-    button.classList.add("twoTimesButton");
+
+    function handlePlaybackRateStatus (rate) {
+      const playbackRate = Number(rate);
+      if(playbackRate === 1) {
+        button.innerHTML = "2x";
+        button.classList.add("twoTimesButton");
+        button.classList.remove("oneTimeButton");
+        localStorage.setItem("playbackRate", "1");
+      } else if (playbackRate === 2) {
+        button.innerHTML = "1x";
+        button.classList.add("oneTimeButton");
+        button.classList.remove("twoTimesButton");
+        localStorage.setItem("playbackRate", "2");
+      }
+      return playbackRate;
+    }
+
+    function handleAudioPlaybackRate (rate = 1) {
+      const playbackRate = Number(rate);
+      document.addEventListener("click", () => {
+        const audios = document.querySelectorAll("audio");
+        if(audios) {
+          audios.forEach(audio => {
+            audio.playbackRate = playbackRate;
+          })
+        }
+      })
+    }
+
+    if(!localStorage.getItem("playbackRate")) {
+      localStorage.setItem("playbackRate", "1");
+      handlePlaybackRateStatus("1");
+    } else {
+      const playbackRate = handlePlaybackRateStatus(localStorage.getItem("playbackRate"));
+      handleAudioPlaybackRate(playbackRate);
+    }
 
     button.addEventListener("click", () => {
-      const audios = document.querySelectorAll("audio");
+      let playbackRate = Number(localStorage.getItem("playbackRate"));
       if (playbackRate === 1) {
-        button.innerHTML = "1x";
         playbackRate++;
-        button.classList.remove("twoTimesButton");
-        button.classList.add("oneTimeButton");
+        playbackRate = playbackRate = handlePlaybackRateStatus(playbackRate);
+        handleAudioPlaybackRate(playbackRate);
       } else if (playbackRate === 2) {
-        button.innerHTML = "2x";
         playbackRate--;
-        button.classList.remove("oneTimeButton");
-        button.classList.add("twoTimesButton");
+        playbackRate = playbackRate = handlePlaybackRateStatus(playbackRate);
+        handleAudioPlaybackRate(playbackRate);
       }
-      audios.forEach((audio) => {
-        audio.playbackRate = playbackRate;
-      });
     });
 
     header.appendChild(button);
